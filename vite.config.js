c866e3bd2912668749c1b1d5bb0653bfb1f3b119
maxommsk <119955040+maxommsk@users.noreply.github.com>
2025-06-26 13:27:10 +0300

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,18 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
 
-// https://vite.dev/config/
+// https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
-})
+  plugins: [react( )],
+  resolve: {
+    alias: {
+      '@': '/src', // Добавляем алиас для @
+    },
+  },
+  // Конфигурация Vitest
+  test: {
+    globals: true, // Делает expect, test, describe глобальными
+    environment: 'jsdom', // Использует JSDOM для эмуляции DOM-окружения браузера
+    setupFiles: './tests/setup.js', // Файл для настройки тестовой среды
+  },
+});
