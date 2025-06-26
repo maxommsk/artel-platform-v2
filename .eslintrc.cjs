5b16b9023ada5542189181af7e68072d479fa3ab
maxommsk <119955040+maxommsk@users.noreply.github.com>
2025-06-26 13:07:39 +0300

    Create .eslintrc.cjs

---
+++ .eslintrc.cjs
@@ -0,0 +1,26 @@
+module.exports = {
+  root: true,
+  env: { browser: true, es2020: true },
+  extends: [
+    'eslint:recommended',
+    'plugin:react/recommended',
+    'plugin:react-hooks/recommended',
+    'plugin:jsx-a11y/recommended',
+    'plugin:import/errors',
+    'plugin:import/warnings',
+  ],
+  ignorePatterns: ['dist', '.eslintrc.cjs'],
+  parser: '@typescript-eslint/parser',
+  parserOptions: { ecmaVersion: 'latest', sourceType: 'module' },
+  settings: { react: { version: '18.2' } },
+  plugins: ['react-refresh'],
+  rules: {
+    'react-refresh/only-export-components': [
+      'warn',
+      { allowConstantExport: true },
+    ],
+    'react/prop-types': 'off', // Отключаем проверку prop-types, если используете TypeScript или не хотите ее
+    'no-unused-vars': 'warn', // Предупреждение о неиспользуемых переменных
+    'jsx-a11y/anchor-is-valid': 'off', // Отключаем, если используете Link из react-router-dom
+  },
+};
