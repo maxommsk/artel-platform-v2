47af9880e0e4dacff948bbfa00c96300b60e7c0c
maxommsk <119955040+maxommsk@users.noreply.github.com>
2025-06-26 13:10:04 +0300

    Update .eslintrc.cjs

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -5,22 +5,40 @@ module.exports = {
     'eslint:recommended',
     'plugin:react/recommended',
     'plugin:react-hooks/recommended',
-    'plugin:jsx-a11y/recommended',
-    'plugin:import/errors',
-    'plugin:import/warnings',
+    'plugin:@typescript-eslint/recommended', // Добавляем правила для TypeScript
+    'plugin:jsx-a11y/recommended', // Добавляем правила доступности
+    'plugin:import/errors', // Проверка ошибок импорта
+    'plugin:import/warnings', // Проверка предупреждений импорта
   ],
   ignorePatterns: ['dist', '.eslintrc.cjs'],
-  parser: '@typescript-eslint/parser',
-  parserOptions: { ecmaVersion: 'latest', sourceType: 'module' },
-  settings: { react: { version: '18.2' } },
-  plugins: ['react-refresh'],
+  parser: '@typescript-eslint/parser', // Используем TypeScript парсер
+  parserOptions: { 
+    ecmaVersion: 'latest', 
+    sourceType: 'module',
+    ecmaFeatures: {
+      jsx: true, // Включаем поддержку JSX
+    },
+  },
+  settings: { 
+    react: { 
+      version: 'detect' // Автоматически определяет версию React
+    },
+    'import/resolver': {
+      node: {
+        extensions: ['.js', '.jsx', '.ts', '.tsx'],
+      },
+    },
+  },
+  plugins: ['react-refresh', '@typescript-eslint', 'import'], // Добавляем плагины
   rules: {
     'react-refresh/only-export-components': [
       'warn',
       { allowConstantExport: true },
     ],
-    'react/prop-types': 'off', // Отключаем проверку prop-types, если используете TypeScript или не хотите ее
+    'react/prop-types': 'off', // Отключаем проверку prop-types, так как обычно используется TypeScript или она не нужна
     'no-unused-vars': 'warn', // Предупреждение о неиспользуемых переменных
-    'jsx-a11y/anchor-is-valid': 'off', // Отключаем, если используете Link из react-router-dom
+    'jsx-a11y/anchor-is-valid': 'off', // Отключаем, если используете Link из react-router-dom и не хотите, чтобы ESLint ругался на отсутствие href
+    '@typescript-eslint/no-unused-vars': 'warn', // Правило для TypeScript о неиспользуемых переменных
+    '@typescript-eslint/no-explicit-any': 'off', // Отключаем, если не хотите ругаться на 'any'
   },
 };
