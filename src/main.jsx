70fb8bfa7eb056e4a1e211099dfb50b321ad990a
maxommsk <119955040+maxommsk@users.noreply.github.com>
2025-06-27 02:18:30 +0300

    Update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -1,9 +1,11 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
-import App from './App.jsx'
+import App from './App'
 
-createRoot(document.getElementById('root')).render(
+const container = document.getElementById('root')!
+
+createRoot(container).render(
   <StrictMode>
     <App />
   </StrictMode>,
